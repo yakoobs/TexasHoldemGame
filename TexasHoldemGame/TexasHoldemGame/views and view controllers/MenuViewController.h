fa501d6413ab8de5d21df676a4d62ece5019e87c
Jakub Sokolowski <Jakub.Sokolowski@monitise.com>
2013-11-13 23:45:24 +0100

    Added Menu view controller. Added Storyboard. App in landscape mode

---
+++ TexasHoldemGame/TexasHoldemGame/views and view controllers/MenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MenuViewController.h
+//  TexasHoldemGame
+//
+//  Created by Jakub Sokolowski on 13/11/2013.
+//  Copyright (c) 2013 WUT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuViewController : UIViewController
+
+@end
---
+++ TexasHoldemGame/TexasHoldemGame/views and view controllers/MenuViewController.m
@@ -0,0 +1,38 @@
+//
+//  MenuViewController.m
+//  TexasHoldemGame
+//
+//  Created by Jakub Sokolowski on 13/11/2013.
+//  Copyright (c) 2013 WUT. All rights reserved.
+//
+
+#import "MenuViewController.h"
+
+@interface MenuViewController ()
+
+@end
+
+@implementation MenuViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
