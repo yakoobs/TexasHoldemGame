0d31c9c5676ab6d362096d2aa64a17bb30749720
Jakub Sokolowski <Jakub.Sokolowski@monitise.com>
2013-11-13 22:24:50 +0100

    Initial project files and project settings

---
+++ TexasHoldemGame/TexasHoldemGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TexasHoldemGame
+//
+//  Created by Jakub Sokolowski on 13/11/2013.
+//  Copyright (c) 2013 WUT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TexasHoldemGame/TexasHoldemGameTests/TexasHoldemGameTests.m
@@ -0,0 +1,34 @@
+//
+//  TexasHoldemGameTests.m
+//  TexasHoldemGameTests
+//
+//  Created by Jakub Sokolowski on 13/11/2013.
+//  Copyright (c) 2013 WUT. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TexasHoldemGameTests : XCTestCase
+
+@end
+
+@implementation TexasHoldemGameTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
