9502d69c7cadb00dc6f25e1618f7946982831036
Jakub Sokolowski <Jakub.Sokolowski@monitise.com>
2013-11-14 00:15:57 +0100

    Added empty Host, Join, Settings, Info view controllers

---
+++ TexasHoldemGame/TexasHoldemGame/InfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  InfoViewController.h
+//  TexasHoldemGame
+//
+//  Created by Jakub Sokolowski on 14/11/2013.
+//  Copyright (c) 2013 WUT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfoViewController : UIViewController
+
+@end
---
+++ TexasHoldemGame/TexasHoldemGame/InfoViewController.m
@@ -0,0 +1,38 @@
+//
+//  InfoViewController.m
+//  TexasHoldemGame
+//
+//  Created by Jakub Sokolowski on 14/11/2013.
+//  Copyright (c) 2013 WUT. All rights reserved.
+//
+
+#import "InfoViewController.h"
+
+@interface InfoViewController ()
+
+@end
+
+@implementation InfoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
