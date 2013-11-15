e963e30a40ac7d0598c89bb8b33bea92a25cc05b
Jakub Sokolowski <Jakub.Sokolowski@monitise.com>
2013-11-15 02:06:04 +0100

    Added Loading Game Settings View controller to project and storyboard.

---
+++ TexasHoldemGame/TexasHoldemGame/views and view controllers/game/LoadingGameSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoadingGameSettingsViewController.h
+//  TexasHoldemGame
+//
+//  Created by Jakub Sokolowski on 15/11/2013.
+//  Copyright (c) 2013 WUT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoadingGameSettingsViewController : UIViewController
+
+@end
---
+++ TexasHoldemGame/TexasHoldemGame/views and view controllers/game/LoadingGameSettingsViewController.m
@@ -0,0 +1,38 @@
+//
+//  LoadingGameSettingsViewController.m
+//  TexasHoldemGame
+//
+//  Created by Jakub Sokolowski on 15/11/2013.
+//  Copyright (c) 2013 WUT. All rights reserved.
+//
+
+#import "LoadingGameSettingsViewController.h"
+
+@interface LoadingGameSettingsViewController ()
+
+@end
+
+@implementation LoadingGameSettingsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
